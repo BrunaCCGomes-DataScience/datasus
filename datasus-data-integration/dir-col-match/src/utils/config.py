8a385af4ef63fd4066faf516aba9446e4b185d3e
brunaccgomes <brunacunha_ti@outlook.com>
2024-07-03 04:32:44 -0300

    Add lista sigla_uf e sua referencia no init

--- datasus-data-integration/dir-col-match/src/utils/config.py
+++ datasus-data-integration/dir-col-match/src/utils/config.py
@@ -1,4 +1,12 @@
+# data_integration/utils/config.py
+
 import os
+
+SIGLA_UF = [
+    'ac', 'al', 'ap', 'am', 'ba', 'ce', 'df', 'es', 'go', 'ma', 'mt', 'ms',
+    'mg', 'pa', 'pb', 'pr', 'pe', 'pi', 'rj', 'rn', 'rs', 'ro', 'rr', 'sc',
+    'sp', 'se', 'to'
+]
 
 class Config:
     def __init__(self):
@@ -6,3 +14,4 @@ class Config:
         self.directory_path = os.getenv('DIRECTORY_PATH', 'data/input/')
         self.destination_path = os.getenv('DESTINATION_PATH', 'data/output/')
         self.column_separator = os.getenv('COLUMN_SEPARATOR', ',')
+        self.sigla_uf = os.getenv('SIGLA_UF', 'sp').lower()
